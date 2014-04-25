6b7e1d4172cc15482117a8761ed55296b4c7e345
Jim Mainprice <mainprice@gmail.com>
2014-04-25 19:48:44 -0400

    Add author

--- src/voxelcolcheck.hpp
+++ src/voxelcolcheck.hpp
@@ -12,6 +12,9 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/** \author Dmitry Berenson and Jim Mainprice */
+
 #ifndef VOXELCOLCHECK_H
 #define VOXELCOLCHECK_H
 
